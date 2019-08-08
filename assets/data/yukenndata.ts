94650f4a411bd2078ba4938fea7f9395d1e3385c
KennithYu <52869485+KennithYu@users.noreply.github.com>
2019-08-07 22:53:39 -0400

    Add files via upload

---
+++ assets/data/yukenndata.ts
@@ -0,0 +1,9 @@
+import { yukenn } from '../../app/yukenn';
+
+export const YUKENNDATA: yukenn = {
+    sid: 991465491,
+    sname: 'Kennith King Hay Yu',
+    slogin: 'yukenn',
+    scampus: 'Davis Campus',
+    atitle: 'Assignment 4'
+}
